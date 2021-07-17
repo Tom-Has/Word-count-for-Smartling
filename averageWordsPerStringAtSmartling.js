476b03b8a26c199e2e7d5a5bf9b53f6ada31f03b
Tom-Has <75426771+Tom-Has@users.noreply.github.com>
2021-07-17 17:41:23 +0200

    Update averageWordsPerStringAtSmartling.js

--- averageWordsPerStringAtSmartling.js
+++ averageWordsPerStringAtSmartling.js
@@ -54,3 +54,28 @@
 		return +(wordCountTotal / stringNumber).toFixed(2);
 			
 	}
+
+
+/*
+* compact version of the above function with an adapted return statement so that the total words per page is returned
+*/
+
+function totalWordsOfPageAtSmartling() {
+	var stringCollection = document.getElementsByClassName("segments__source___2Vmtne");
+	var stringNumber = stringCollection.length;
+	var currentString = "";
+	var wordCountTotal = 0;
+	var wordsPerString = 0;
+	var charsPerString = 0;
+	var averageWordsPerString = 0;
+	for(var i = 0; i < stringNumber; ++i) {
+		wordsPerString = 1;
+		currentString = stringCollection[i].innerText;
+		charsPerString = currentString.length;
+		for(var j = 0; j < charsPerString; ++j)
+			if(currentString.charCodeAt(j) == 32 || currentString.charCodeAt(j) == 9166)
+				++wordsPerString;
+		wordCountTotal += wordsPerString;
+	}
+	return wordCountTotal;
+}
